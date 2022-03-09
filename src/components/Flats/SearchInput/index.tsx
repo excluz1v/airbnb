2de974a5774d71c9714024069daf9651d3ce7285
Dmitriy <bmd4work@gmail.com>
2022-03-09 20:38:30 +0600

    refactor: change width of flat component

---
+++ src/components/Flats/SearchInput/index.tsx
@@ -0,0 +1,12 @@
+import { TextField } from '@mui/material';
+import React from 'react';
+
+function SearchInput() {
+  return (
+    <TextField variant="filled" label="city" placeholder="type something">
+      SearchInput
+    </TextField>
+  );
+}
+
+export default SearchInput;
--- src/components/Flats/index.tsx
+++ src/components/Flats/index.tsx
@@ -4,13 +4,21 @@ import { Box } from '@mui/system';
 import MenuBar from '../MenuBar';
 import FlatCard from './FlatCard';
 import flatList from '../../flatsList';
+import SearchInput from './SearchInput';
 
 function Flats() {
   return (
     <Grid container>
       <Grid item xs={12}>
         <MenuBar />
-        <Box display="flex" flexDirection="column" rowGap={5} p={3}>
+        <Box
+          display="flex"
+          flexDirection="column"
+          rowGap={5}
+          p={3}
+          maxWidth={580}
+        >
+          <SearchInput />
           {flatList.map((flat) => {
             return <FlatCard {...flat} key={flat.id} />;
           })}
