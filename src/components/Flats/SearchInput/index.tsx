7fa792e34dffd043d1b8f750fe950d485c6745a6
Dmitriy <bmd4work@gmail.com>
2022-03-09 21:05:49 +0600

    feat: add .env file with api key

--- src/components/Flats/SearchInput/index.tsx
+++ src/components/Flats/SearchInput/index.tsx
@@ -1,7 +1,22 @@
 import { TextField } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function SearchInput() {
+  useEffect(() => {
+    async function getLibrary() {
+      try {
+        const library = await fetch(
+          `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}&libraries=places&callback=initMap`,
+        );
+        return library;
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
+    }
+    // const library = getLibrary();
+  }, []);
+
   return (
     <TextField variant="filled" label="city" placeholder="type something">
       SearchInput
