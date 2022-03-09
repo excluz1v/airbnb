2e42a65ed4b777c8d75fad4bc4c251819e8efa94
Dmitriy <bmd4work@gmail.com>
2022-03-09 15:08:00 +0600

    refactor: move menubar to own component

--- src/components/Flats/index.tsx
+++ src/components/Flats/index.tsx
@@ -1,7 +1,15 @@
+import { Grid } from '@mui/material';
 import React from 'react';
+import MenuBar from '../MenuBar';
 
 function Flats() {
-  return <div>Flats</div>;
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <MenuBar />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Flats;
