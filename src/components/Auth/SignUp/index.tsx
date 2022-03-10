911f71c47e31e61cbadd4c9b1738e76cecf45424
Dmitriy <bmd4work@gmail.com>
2022-03-10 20:40:12 +0600

    refactor: move signup logic to auth folder

---
+++ src/components/Auth/SignUp/index.tsx
@@ -0,0 +1,32 @@
+import { Grid, Typography } from '@mui/material';
+import React from 'react';
+import Logo from '../../Unknown/Logo/Logo';
+import ScreenToggler from '../../Unknown/ScreenToggler';
+import RegForm from '../SignUpForm';
+
+const Registration: React.FC = () => {
+  return (
+    <>
+      <Grid justifyContent="center" item xs={12}>
+        <Logo />
+      </Grid>
+      <Grid textAlign="center" item xs={12}>
+        <Typography variant="h2" fontWeight="bold">
+          Register
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <RegForm />
+      </Grid>
+      <Grid item xs={12}>
+        <ScreenToggler
+          description="Already have account?"
+          linkText="login"
+          linkTo="/login"
+        />
+      </Grid>
+    </>
+  );
+};
+
+export default Registration;
