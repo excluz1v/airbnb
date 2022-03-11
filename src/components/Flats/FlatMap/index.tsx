b6d55c4062f12b3b81f73dad201bbc327d07fe17
Dmitriy <bmd4work@gmail.com>
2022-03-11 15:08:00 +0600

    feat: add styles to FlatMap

--- src/components/Flats/FlatMap/index.tsx
+++ src/components/Flats/FlatMap/index.tsx
@@ -1,7 +1,21 @@
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 function FlatMap(): JSX.Element {
-  return <div>FlatMap</div>;
+  return (
+    <Box
+      bgcolor="#BDBDBD"
+      height="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      position="sticky"
+      top={0}
+      color="#FFF"
+    >
+      <Typography variant="h4">No flat selected</Typography>
+    </Box>
+  );
 }
 
 export default FlatMap;
