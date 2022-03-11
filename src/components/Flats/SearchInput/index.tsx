322baf247e734a085be9897e65d94fda740463ff
Dmitriy <bmd4work@gmail.com>
2022-03-11 16:06:26 +0600

    fix: remove eslint warnings

---
+++ src/components/Flats/SearchInput/index.tsx
@@ -0,0 +1,131 @@
+import { Search } from '@mui/icons-material';
+import {
+  FilledInput,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+} from '@mui/material';
+import React, { useEffect, useRef } from 'react';
+
+function loadAsyncScript(url: string) {
+  return new Promise((resolve) => {
+    const script = document.createElement('script');
+    Object.assign(script, {
+      type: 'text/javascript',
+      async: true,
+      src: url,
+    });
+    script.addEventListener('load', () => resolve(script));
+    document.head.appendChild(script);
+  });
+}
+
+const initMapScript = () => {
+  // if script already loaded
+  const properties = Object.getOwnPropertyNames(window);
+  if (properties.includes('google')) {
+    return Promise.resolve();
+  }
+  const src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}&language=en&libraries=places`;
+  return loadAsyncScript(src);
+};
+
+const extractAddress = (place: google.maps.places.PlaceResult) => {
+  const address = {
+    city: '',
+    country: '',
+    plain() {
+      const city = this.city ? `${this.city}, ` : '';
+      return city + this.country;
+    },
+  };
+
+  if (!Array.isArray(place?.address_components)) {
+    return address;
+  }
+
+  place.address_components.forEach((component) => {
+    const { types } = component;
+    const value = component.long_name;
+
+    if (types.includes('locality')) {
+      address.city = value;
+    }
+
+    if (types.includes('country')) {
+      address.country = value;
+    }
+  });
+  return address;
+};
+
+type Tprops = {
+  onChange: (s: string) => void;
+  value: string;
+};
+
+function SearchInput(props: Tprops): JSX.Element {
+  const { value, onChange } = props;
+
+  const searchInput = useRef<HTMLInputElement>(null);
+
+  // do something on address change
+  const onChangeAddress = (autocomplete: google.maps.places.Autocomplete) => {
+    const place = autocomplete.getPlace();
+    const cityAndCountry = extractAddress(place).plain();
+    onChange(cityAndCountry);
+  };
+
+  function onChangeHandler(
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) {
+    onChange(e.target.value);
+  }
+  // init autocomplete
+  const initAutocomplete = () => {
+    if (!searchInput.current) return false;
+
+    const autocomplete = new window.google.maps.places.Autocomplete(
+      searchInput.current,
+    );
+    autocomplete.setFields(['address_component', 'geometry']);
+    autocomplete.addListener('place_changed', () =>
+      onChangeAddress(autocomplete),
+    );
+    return true;
+  };
+
+  useEffect(() => {
+    initMapScript().then(() => {
+      initAutocomplete();
+    });
+  });
+
+  return (
+    <FormControl variant="filled" fullWidth>
+      <InputLabel htmlFor="search-city">City</InputLabel>
+      <FilledInput
+        inputComponent="input"
+        value={value}
+        onChange={(e) => onChangeHandler(e)}
+        inputRef={searchInput}
+        placeholder="type something"
+        id="search-city"
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="search"
+              onClick={() => onChange(value)}
+              edge="end"
+            >
+              <Search />
+            </IconButton>
+          </InputAdornment>
+        }
+      />
+    </FormControl>
+  );
+}
+
+export default SearchInput;
---
+++ src/components/Unknown/Inputs/PasswordInput.tsx
@@ -0,0 +1,56 @@
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  TextField,
+} from '@mui/material';
+import React, { useState } from 'react';
+
+type Tprops = {
+  value: string;
+  onChange: (e: React.ChangeEvent) => void;
+  errors: string | undefined;
+  label: string;
+  name: string;
+};
+
+function PasswordInput(props: Tprops): JSX.Element {
+  const { value, onChange, errors, label, name } = props;
+  const id = `${name}-error-text`;
+  const [showPass, setShowPass] = useState(false);
+  const handleTogglePassword = () => {
+    setShowPass(!showPass);
+  };
+  return (
+    <FormControl fullWidth error>
+      <TextField
+        name={name}
+        type={showPass ? 'text' : 'password'}
+        onChange={onChange}
+        value={value}
+        variant="filled"
+        label={label}
+        size="small"
+        error={!!errors}
+        helperText={errors}
+        aria-describedby={id}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="start">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleTogglePassword}
+                edge="end"
+              >
+                {!showPass ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </FormControl>
+  );
+}
+
+export default PasswordInput;
